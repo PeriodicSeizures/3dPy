47447c406f98a2dade10b6631dc9a29c266eb3a8
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2019-03-07 10:11:06 -0500

    Update main.py

--- 3.0/main.py
+++ 3.0/main.py
@@ -6,17 +6,32 @@ import json
 pygame.init()
 clock = pygame.time.Clock()
 
-game = gl.gl_engine(600, 600)
+renderer = gl.renderer(600, 600)
 camera = gl.camera()
 
-file_name = "map.json"
-objects = []
-with open(file_name) as file:
-    objects = json.load(file)
-    print(objects)
+with open("map.json") as f:
+    objects = json.load(f)
+    renderer.addObjectArray(objects)
+    #print(objects)
 
 run = True
 while(run):
     delta = clock.tick()/1000
-    game.update(clock, delta)
+    renderer.render(clock, delta)
     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: running = False; break
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: pygame.quit()
+
+            if event.key == pygame.K_f:
+                if focus:
+                    pygame.event.set_grab(0)
+                    pygame.mouse.set_visible(1)
+                else:
+                    pygame.event.set_grab(1)
+                    pygame.mouse.set_visible(0)
+                focus = not focus
+        if focus:
+            camera.events(event)
