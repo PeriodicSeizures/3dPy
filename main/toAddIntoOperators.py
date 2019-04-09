50de5fda9c4c89d31866a74896ccd36bf599e895
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2019-04-09 16:07:34 -0400

    n
    n

--- main/toAddIntoOperators.py
+++ main/toAddIntoOperators.py
@@ -32,11 +32,88 @@ def volumeOfTetrahedron(vert, vert2, vert3, vert4):
     h = #heighest pt to base
     
 
+def distOf3dVertPair(vert1, vert2):
+    d = math.sqrt(
 
-def pointIn3dTri(args):
-    x∗(P1−P0)+y∗(P2−P0)=P−P0
+            math.pow(vert1[0]-vert2[0], 2) +
+
+            math.pow(vert1[1]-vert2[1], 2) +
+
+            math.pow(vert1[2]-vert2[2], 2)
+
+
+        )
+    return d
+
+def pointIn3dTri(point, tri):
+    # x∗(P1−P0)+y∗(P2−P0)=P−P0
     # https://math.stackexchange.com/questions/4322/check-whether-a-point-is-within-a-3d-triangle
+
     
+
+    AreaABC = areaOfTri(tri[0][0])
+
+    P = [point[0],point[1],point[2]]
+
+    A = tri[0]; B = tri[1]; C = tri[2]
+
+    a = (
+        (distOf3dVertPair(P, B) * distOf3dVertPair(P, C)) /
+        (2 * )
+        )
+
+"""
+
+    Instead of finding exact collisions with weird math,
+    just use raycasts at fixed intervals,
+    and fool-proof collisions
+    (as in :
+
+        ray was casted, and found a tri X units ahead, which means that
+
+        object will collide within some time.
+
+        After continued casting, object usually and repeatedly found by cast
+
+        is no longer located after a raycast.
+
+        This most likely means that :
+
+            the object moved out of the raycast,
+            the rays origin passed the object
+            ...
+        
+        )
+
+
+
+    ::::::::::::::::::::::
+    Pseudocode::::
+
+        cast ray and extend from there, testing for collisions
+        If collide, then record distance traveled,
+
+            pros:
+
+                *easiest* method of collsiions to implement
+
+            cons:
+
+                *very innacurate, ray might pass object/tri's
+
+        OR:::::
+
+        test for wall approximately X units ahead,
+        which is equal to velocity as a vector
+
+        so, the faster self is moving, the greater distance away to check
+        and the slower, the smaller distance to check for object/tri...
+
+        OR:::::::
+
+        just test like .1   OR   1 units
+        
+
+
+"""
     
-# possible
-# https://stackoverflow.com/questions/995445/determine-if-a-3d-point-is-within-a-triangle
