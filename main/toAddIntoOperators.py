c449bd32a4a22cefd1e64dead1787b9234549517
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2019-04-10 10:11:34 -0400

    Update toAddIntoOperators.py

--- main/toAddIntoOperators.py
+++ main/toAddIntoOperators.py
@@ -1,3 +1,52 @@
+"""
+    
+    NEXT TO ADD:::
+
+"""
+
+def determinant_3x3(m):
+    return (m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) -
+            m[1][0] * (m[0][1] * m[2][2] - m[0][2] * m[2][1]) +
+            m[2][0] * (m[0][1] * m[1][2] - m[0][2] * m[1][1]))
+
+
+def subtract(a, b):
+    return (a[0] - b[0],
+            a[1] - b[1],
+            a[2] - b[2])
+
+def tetrahedron_calc_volume(a, b, c, d):
+    return (abs(determinant_3x3((subtract(a, b),
+                                 subtract(b, c),
+                                 subtract(c, d),
+                                 ))) / 6.0)
+
+a = [0.0, 0.0, 0.0]
+d = [2.0, 0.0, 0.0]
+c = [0.0, 2.0, 0.0]
+b = [0.0, 0.0, 2.0]
+
+print(tetrahedron_calc_volume(a, b, c, d))
+
+
+# https://stackoverflow.com/questions/9866452/calculate-volume-of-any-tetrahedron-given-4-points
+
+# https://stackoverflow.com/questions/53962225/how-to-know-if-a-line-segment-intersects-a-triangle-in-3d-space
+
+###########################################
+
+
+
+
+
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+
+
 def areaOfTri(x1, y1, x2, y2, x3, y3): 
     return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0) 
 
