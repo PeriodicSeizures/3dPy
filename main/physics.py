23c0f14a6004ec513dd7bc98c9b175780b389da2
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2019-04-15 07:43:42 -0400

    c
    c

--- main/physics.py
+++ main/physics.py
@@ -4,6 +4,7 @@ import gl
 import math2d
 import math3d
 import numpy as np
+import copy
 
 class physics:
     def __init__(self):
@@ -13,72 +14,77 @@ class physics:
     def update(self, delta):
         if self.isActive:
             for o1 in gl.gameObjects:
+
+
+
+
+
                 if o1.isKinetic:
                     xBreak = False; yBreak = False; zBreak = False
                     
                     n = math3d.normalize(o1.velocity)
                     x = o1.pos[0]; y = o1.pos[1]; z = o1.pos[2]
 
+        
+                    """
+
+                            GRAVITY
+
+                    """
+                    
                     if o1.useGravity:
                         o1.velocity[1] += self.gravity*delta
+
+
+
+
+                    """
+
+                    CHECK FOR COLLISIONS
+
+                    """
                     
                     for o2 in gl.gameObjects:
-                        
                         if o1.name != o2.name:
+                       
                             for face in o2.faces:
-                                tri = face["verts"]
-
-                                """
-
-                                        GRAVITY
-
-                                """
-                                
-
-
-
-                                    
-
-                                """
-
-                                        COLLISIONS
-                                
-                                """
-                                if xBreak and yBreak and zBreak:
-                                    break
-                                
-
-                                if n != -1:
-                                    if not xBreak:
-                                        if math3d.pointInTri(x+n[0]/10, y, z,
-                                                             tri[0][0],tri[0][1],tri[0][2],
-                                                             tri[1][0],tri[1][1],tri[1][2],
-                                                             tri[2][0],tri[2][1],tri[2][2]):
-                                            
-                                            xBreak = True
-
-                                            
-                                    if not yBreak:
-                                        if math3d.pointInTri(x, y+n[0]/10, z,
-                                                             tri[0][0],tri[0][1],tri[0][2],
-                                                             tri[1][0],tri[1][1],tri[1][2],
-                                                             tri[2][0],tri[2][1],tri[2][2]):
-                                            
-                                            if n[1] < 0:
-                                                o1.grounded = True
-                                            else:
-                                                o1.grounded = False
-                                            
-                                            yBreak = True
+                                tri = copy.deepcopy(face["verts"])
+                                print(tri)
+                                for v in range(len(tri)):
+                                    tri[v][0] += x
+                                    tri[v][1] += y
+                                    tri[v][2] += z
+                                   
+                                if o1.velocity[0]!=0:
+                                    if math3d.pointInTri(x+n[0]/10, y, z,
+                                                         tri[0][0],tri[0][1],tri[0][2],
+                                                         tri[1][0],tri[1][1],tri[1][2],
+                                                         tri[2][0],tri[2][1],tri[2][2]):
+                                        
+                                        o1.velocity[0] = 0 #math2d.clamp(o1.velocity[0], 0, -o1.velocity[0])
 
                                         
-                                    if not zBreak:
-                                        if math3d.pointInTri(x, y, z+n[0]/10,
-                                                             tri[0][0],tri[0][1],tri[0][2],
-                                                             tri[1][0],tri[1][1],tri[1][2],
-                                                             tri[2][0],tri[2][1],tri[2][2]):
-                                            
-                                            zBreak = True
+                                if o1.velocity[1]!=0:
+                                    if math3d.pointInTri(x, y+n[0]/10, z,
+                                                         tri[0][0],tri[0][1],tri[0][2],
+                                                         tri[1][0],tri[1][1],tri[1][2],
+                                                         tri[2][0],tri[2][1],tri[2][2]):
+                                        
+                                        if n[1] < 0:
+                                            o1.grounded = True
+                                        else:
+                                            o1.grounded = False
+                                        
+                                        o1.velocity[1] = 0 #math2d.clamp(o1.velocity[1], 0, -o1.velocity[1])
+
+                                    
+                                if o1.velocity[2]!=0:
+                                    if math3d.pointInTri(x, y, z+n[0]/10,
+                                                         tri[0][0],tri[0][1],tri[0][2],
+                                                         tri[1][0],tri[1][1],tri[1][2],
+                                                         tri[2][0],tri[2][1],tri[2][2]):
+                                        
+                                        o1.velocity[2] = 0 #math2d.clamp(o1.velocity[2], 0, -o1.velocity[2])
 
 
 
@@ -140,13 +146,6 @@ class physics:
                             MOVE OBJECT BY VELOCITY
 
                     """
-
-                    if xBreak:
-                        o1.velocity[0] = 0 #math2d.clamp(o1.velocity[0], 0, -o1.velocity[0])
-                    if yBreak:
-                        o1.velocity[1] = 0 #math2d.clamp(o1.velocity[1], 0, -o1.velocity[1])
-                    if zBreak:
-                        o1.velocity[2] = 0 #math2d.clamp(o1.velocity[2], 0, -o1.velocity[2])
 
                     if o1.velocity[0]!=0:
                         o1.pos[0] += .5*(o1.velocity[0])*delta
