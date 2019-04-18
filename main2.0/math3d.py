e03025c5a171b4110e8ee00cf1cb1a08bf139d7f
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2019-04-18 13:31:21 -0400

    Update math3d.py

--- main2.0/math3d.py
+++ main2.0/math3d.py
@@ -95,3 +95,57 @@ def normalize(vector):
     d = dist([0,0,0], vector)
     v = [vector[0]/d, vector[1]/d, vector[2]/d]
     return v
+
+
+def uVectNorm(x1,y1,z1,    # P
+              x2,y2,z2,    # Q
+              x3,y3,z3):   # R
+    """
+    PQ = [x2-x1,y2-y1,z2-z1]
+    PR = [x3-x1,y3-y1,z3-z1]
+
+    x = np.array([[PQ[1], PQ[2]],
+                  [PR[1], PR[2]]])
+    y = np.array([[PQ[0], PQ[2]],
+                  [PR[0], PR[2]]])
+    z = np.array([[PQ[0], PQ[1]],
+                  [PR[0], PR[1]]])
+    
+    x = np.linalg.det(x)
+    y = np.linalg.det(y)
+    z = np.linalg.det(z)
+
+    return x,y,z
+    """
+    p1 = np.array([x1,y1,z1])
+    p2 = np.array([x2,y2,z2])
+    p3 = np.array([x3,y3,z3])
+
+    v1 = p3-p1
+    v2 = p2-p1
+
+    cp = np.cross(v1,v2)
+    a,b,c = cp
+
+    d = np.dot(cp, p3)
+
+    print(a,b,c)
+
+"""
+print(uVectNorm(1,-1,1,
+                4,3,7,
+                0,0,0))
+"""
+"""
+print(uVectNorm(3,-1,2,
+                1,-1,-3,
+                4,-3,1))
+"""
+"""
+print(uVectNorm(1,2,3,
+                4,6,9,
+                12,11,9))
+"""
+print(uVectNorm(-1,0,-1,
+                0,0,1,
+                0,0,0))
